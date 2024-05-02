13bcd89fdaf13392d99d600d5ddab4c2bac9baf3
Noah Huggins <noahhugginsx@gmail.com>
2024-05-02 02:02:35 -0500

    Fixed Switch problem

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Home from './components/Home';
 import SignUp from './components/SignUp';
 import SignIn from './components/SignIn';
@@ -7,11 +7,11 @@ import SignIn from './components/SignIn';
 const App = () => {
   return (
     <Router>
-      <Switch>
+      <Routes>
         <Route exact path="/" component={Home} />
         <Route path="/signup" component={SignUp} />
         <Route path="/signin" component={SignIn} />
-      </Switch>
+      </Routes>
     </Router>
   );
 }
